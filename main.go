886dc9d48e34ab82507bfef5593a594250b68583
Fernando <fercho_blk@hotmail.com>
2021-10-17 10:39:52 -0500

    Interfaces go envio notificaciones

---
+++ main.go
@@ -0,0 +1,86 @@
+package main
+
+import "fmt"
+
+type INotificationFactory interface {
+	SederNotification()
+	GetSender() ISender
+}
+
+type ISender interface {
+	GetSenderMethod() string
+	GetSenderChannel() string
+}
+
+type SMSNotification struct {
+}
+
+func (SMSNotification) SederNotification() {
+	fmt.Println("Sender notification via SMS")
+}
+
+func (SMSNotification) GetSender() ISender {
+	return SMSNotificationSender{}
+}
+
+type SMSNotificationSender struct {
+}
+
+func (SMSNotificationSender) GetSenderMethod() string {
+	return "SMS"
+}
+
+func (SMSNotificationSender) GetSenderChannel() string {
+	return "Twilio"
+}
+
+type EmailNotification struct {
+}
+
+func (EmailNotification) SederNotification() {
+	fmt.Println("Sender notification via Email")
+}
+
+func (EmailNotification) GetSender() ISender {
+	return EmailNotificationSender{}
+}
+
+type EmailNotificationSender struct {
+}
+
+func (EmailNotificationSender) GetSenderMethod() string {
+	return "Email"
+}
+
+func (EmailNotificationSender) GetSenderChannel() string {
+	return "getSender"
+}
+
+func getNotificationFactory(notificationType string) (INotificationFactory, error) {
+	if notificationType == "SMS" {
+		return &SMSNotification{}, nil
+	}
+
+	if notificationType == "Email" {
+		return &EmailNotification{}, nil
+	}
+	return nil, fmt.Errorf("No notification Type")
+}
+
+func sendNotification(f INotificationFactory) {
+	f.SederNotification()
+}
+
+func getMethod(f INotificationFactory) {
+	fmt.Println(f.GetSender().GetSenderMethod())
+}
+
+func main() {
+	smsFactory, _ := getNotificationFactory("SMS")
+	emailFactory, _ := getNotificationFactory("Email")
+
+	sendNotification(smsFactory)
+	sendNotification(emailFactory)
+	getMethod(smsFactory)
+	getMethod(emailFactory)
+}
